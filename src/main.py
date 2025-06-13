206f3c8173029122d547271000cb7e6f66c51b26
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-06-13 17:40:02 +0500

    Update documentation for clinical deployment

--- src/main.py
+++ src/main.py
@@ -176,3 +176,5 @@ if __name__ == '__main__':
     main()
 
 # Implement active learning for label efficiency [2025-06-12T10:31:30]
+
+# Update documentation for clinical deployment [2025-06-13T17:40:02]
