c008ae9b30ef129dc26c1eac50c9e6c9d060b786
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-09-05 17:12:29 +0500

    Implement ensemble of UNet and DeepLab

--- src/main.py
+++ src/main.py
@@ -218,3 +218,5 @@ if __name__ == '__main__':
 # Implement active learning for label efficiency [2025-08-28T09:47:37]
 
 # Implement ensemble of UNet and DeepLab [2025-08-29T12:35:35]
+
+# Implement ensemble of UNet and DeepLab [2025-09-05T17:12:29]
