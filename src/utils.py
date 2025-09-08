dbdfe3f2489562fe31de8587285531578249bfbd
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-09-08 14:08:23 +0500

    Implement Grad-CAM explainability heatmaps

--- src/utils.py
+++ src/utils.py
@@ -95,3 +95,5 @@ def format_number(n: int) -> str:
 # Implement Grad-CAM explainability heatmaps [2025-09-06T19:40:45]
 
 # Implement class activation mapping overlay [2025-09-08T17:33:49]
+
+# Implement Grad-CAM explainability heatmaps [2025-09-08T14:08:23]
