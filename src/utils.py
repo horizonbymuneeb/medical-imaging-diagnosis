3b8e37cb08aaf169a115fca935f55f178988778c
horizonbymuneeb <horizonbymuneeb@gmail.com>
2025-12-26 17:58:18 +0500

    Update documentation for clinical deployment

--- src/utils.py
+++ src/utils.py
@@ -151,3 +151,5 @@ def format_number(n: int) -> str:
 # Update ResNet classifier for 3D volumes [2025-12-22T15:32:10]
 
 # Add federated learning components for privacy [2025-12-23T17:07:22]
+
+# Update documentation for clinical deployment [2025-12-26T17:58:18]
