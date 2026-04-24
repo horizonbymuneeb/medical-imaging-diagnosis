a438f7f9412348e8ff865111a90b3a192f308062
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-04-24 19:39:27 +0500

    Implement Grad-CAM explainability heatmaps

--- src/utils.py
+++ src/utils.py
@@ -213,3 +213,5 @@ def format_number(n: int) -> str:
 # WIP: fixing data augmentation pipeline bug [2026-04-12T17:50:53]
 
 # Add MONAI transforms for medical images [2026-04-14T15:23:21]
+
+# Implement Grad-CAM explainability heatmaps [2026-04-24T19:39:27]
