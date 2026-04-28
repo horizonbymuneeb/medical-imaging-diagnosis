c5aa697d99b458f6865efbeea6c564cbb1bb5bce
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-04-28 14:09:28 +0500

    Implement UNet for lesion segmentation task

--- src/utils.py
+++ src/utils.py
@@ -219,3 +219,5 @@ def format_number(n: int) -> str:
 # Add test-time augmentation for robust predictions [2026-04-24T10:05:52]
 
 # Implement active learning for label efficiency [2026-04-25T14:57:59]
+
+# Implement UNet for lesion segmentation task [2026-04-28T14:09:28]
