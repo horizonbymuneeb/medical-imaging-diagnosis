137612c873c97399185e03d9112b1415260f0687
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-05-05 18:00:38 +0500

    Add test-time augmentation for robust predictions

--- src/main.py
+++ src/main.py
@@ -334,3 +334,5 @@ if __name__ == '__main__':
 # Implement Grad-CAM explainability heatmaps [2026-04-20T18:39:54]
 
 # Implement class activation mapping overlay [2026-05-02T10:16:50]
+
+# Add test-time augmentation for robust predictions [2026-05-05T18:00:38]
