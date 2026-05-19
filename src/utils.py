e732665d741a3f137b8711adf92d031825df142c
horizonbymuneeb <horizonbymuneeb@gmail.com>
2026-05-19 19:35:47 +0500

    Fix pixel value normalization for CT scans

--- src/utils.py
+++ src/utils.py
@@ -231,3 +231,5 @@ def format_number(n: int) -> str:
 # Implement Grad-CAM explainability heatmaps [2026-05-13T12:09:38]
 
 # Update CI pipeline for medical compliance [2026-05-14T13:52:49]
+
+# Fix pixel value normalization for CT scans [2026-05-19T19:35:47]
